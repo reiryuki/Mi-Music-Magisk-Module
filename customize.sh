55c6ce61c9c1fb03831c5cc742df71dc4c96493c
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-06-09 05:19:55 +0700

    Enhancements

--- customize.sh
+++ customize.sh
@@ -49,10 +49,6 @@ fi
 
 # cleaning
 ui_print "- Cleaning..."
-APP="`ls $MODPATH/system/priv-app` `ls $MODPATH/system/app`"
-for APPS in $APP; do
-  rm -f `find /data/dalvik-cache /data/resource-cache -type f -name *$APPS*`
-done
 rm -rf /metadata/magisk/$MODID
 rm -rf /mnt/vendor/persist/magisk/$MODID
 rm -rf /persist/magisk/$MODID

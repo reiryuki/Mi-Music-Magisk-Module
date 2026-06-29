aaa5996915fc615fe2d69802cd59c299e579785c
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2026-06-29 18:23:41 +0700

    v2.14

--- README.md
+++ README.md
@@ -10,29 +10,92 @@
 
 ## Sources
 - https://apkmirror.com com.miui.player by Mi Music
-- libmagiskpolicy.so: Kitsune Mask R6687BB53
+- libmagiskpolicy.so: Magisk (stable) 30.7 (30700)
+
+## Changelog
+
+v2.14
+- Update libmagiskpolicy.so from Magisk (stable) 30.7 (30700)
+- Resets module folders/files permissions at post-fs-data
+- Move _uninstall.log to /data/adb/logs/
+- Prepare /storage/emulated/"$USR"/Android/data/com.miui.player/files
+
+v2.13
+- Update MiuiMusicGlobal.apk v7.21.01.092709i
+- Fix permissions
+- Minimum SDK 24
+- Add Action button to clear app caches
+- Fix bug in uninstall.sh
+
+v2.12
+- Fix script bug
+
+v2.11
+- Fix permissions
+- Add Dirac EQ UI for ROM supported Dirac soundfx only
+- Fix MagiskHide & SUList
+- Change module name
+
+v2.10
+- Redirect /sdcard to /data/media/\"$UID\"
+- Fix MagiskHide & SUList
+
+v2.9
+- Specify UID at script
+- Add optional debug.log=1 for more detailed install log
+- Move uninstall log to /data/media/.../..._uninstall.log
+
+v2.8
+- KernelSU support
+- Magisk v26.1 support
+- Save install log at /sdcard/..._recovery.log while installing via Recovery
+- Save uninstall log at /data/adb/modules/..._uninstall.log
+- Fix optional permissive mode
+- Sets blacklist/whitelist
+
+v2.7
+- Fix resources conflict in some ROMs
+- Fix sepolicy denials
+- Cleaning protected storage
+- Using sys.boot_completed=1 detection
+
+v2.6
+- Fix sepolicy denials
+- Fix permissions
+- Script enhancements
+
+v2.5
+- package_cache deletion
+- Updated MiuiMusicGlobal.apk version
+- Remove Play Store support
+- Requires Miui Core v4.3-stable or above
+- Allow installation via recovery
+- Minimum SDK 21
+- Fix permissions
+- Removes some ads components
+- Remove miui-stat.jar library
 
 ## Screenshots
-- https://t.me/ryukimodsscreenshots/42
+https://t.me/ryukimodsscreenshots/42
 
 ## Requirements
 - Android 7.0 (SDK 24) and up
-- Magisk or KernelSU installed
+- Magisk or Kitsune Mask or KernelSU or Apatch installed
 - Dirac/Mi Sound EQ UI requires Miui Core Magisk Module installed and ROM supported in-built Dirac/Mi Sound effect
 
 ## Installation Guide & Download Link
+- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings and install https://github.com/KernelSU-Modules-Repo/meta-overlayfs or https://github.com/KernelSU-Modules-Repo/magic_mount_rs or https://github.com/KernelSU-Modules-Repo/hybrid_mount or https://github.com/maxsteeel/nomount first depending on ROM compatibility
 - Install Miui Core Magisk Module first if necessary: https://github.com/reiryuki/Miui-Core-Magisk-Module
-- Install this module https://www.pling.com/p/1535574/ via Magisk app or KernelSU app only
-- If you are using KernelSU, you need to disable Unmount Modules by Default in KernelSU app settings
+- Install this module via Magisk/Kisune Mask/KernelSU/Apatch app only
 - Reboot
 - If you are using KernelSU, you need to allow superuser list manually all package name listed in package.txt (and your home launcher app also) (enable show system apps) and reboot afterwards
 - If you are using SUList, you need to allow list manually your home launcher app (enable show system apps) and reboot afterwards
 
 ## Optionals
-- Global: https://t.me/ryukinotes/35
+Global: https://t.me/ryukinotes/35
 
 ## Troubleshootings
-- Global: https://t.me/ryukinotes/34
+Global: https://t.me/ryukinotes/34
 
 ## Support & Bug Report
 - https://t.me/ryukinotes/54
@@ -43,6 +106,6 @@
 - You can contribute ideas about this Magisk Module here: https://t.me/androidappsportdevelopment
 
 ## Sponsors
-- https://t.me/ryukinotes/25
+https://t.me/ryukinotes/25
 
 
--- service.sh
+++ service.sh
@@ -6,6 +6,8 @@ set -x
 
 # var
 API=`getprop ro.build.version.sdk`
+USR=`id -u`
+[ ! "$USR" ] && USR=0
 
 # property
 resetprop --delete ro.miui.ui.version.name
@@ -38,74 +40,71 @@ fi
 
 # grant
 PKG=com.miui.player
-pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
-pm grant $PKG android.permission.WRITE_EXTERNAL_STORAGE
-if [ "$API" -ge 29 ]; then
-  pm grant $PKG android.permission.ACCESS_MEDIA_LOCATION 2>/dev/null
-  appops set $PKG ACCESS_MEDIA_LOCATION allow
-fi
-if [ "$API" -ge 33 ]; then
-  pm grant $PKG android.permission.READ_MEDIA_AUDIO
-  pm grant $PKG android.permission.READ_MEDIA_VIDEO
-  pm grant $PKG android.permission.READ_MEDIA_IMAGES
-  pm grant $PKG android.permission.POST_NOTIFICATIONS
-  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
-fi
-appops set $PKG LEGACY_STORAGE allow
-appops set $PKG READ_EXTERNAL_STORAGE allow
-appops set $PKG WRITE_EXTERNAL_STORAGE allow
-appops set $PKG READ_MEDIA_AUDIO allow
-appops set $PKG READ_MEDIA_VIDEO allow
-appops set $PKG READ_MEDIA_IMAGES allow
-appops set $PKG WRITE_MEDIA_AUDIO allow
-appops set $PKG WRITE_MEDIA_VIDEO allow
-appops set $PKG WRITE_MEDIA_IMAGES allow
-if [ "$API" -ge 30 ]; then
-  appops set $PKG MANAGE_EXTERNAL_STORAGE allow
-  appops set $PKG NO_ISOLATED_STORAGE allow
-  appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
-fi
-if [ "$API" -ge 31 ]; then
-  pm grant $PKG android.permission.BLUETOOTH_CONNECT
-  appops set $PKG MANAGE_MEDIA allow
-fi
-if [ "$API" -ge 34 ]; then
-  appops set "$PKG" READ_MEDIA_VISUAL_USER_SELECTED allow
-fi
-appops set $PKG SYSTEM_ALERT_WINDOW allow
-appops set $PKG WRITE_SETTINGS allow
-PKGOPS=`appops get $PKG`
-UID=`dumpsys package $PKG 2>/dev/null | grep -m 1 Id= | sed -e 's|    userId=||g' -e 's|    appId=||g'`
-if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
-  appops set --uid "$UID" LEGACY_STORAGE allow
-  appops set --uid "$UID" READ_EXTERNAL_STORAGE allow
-  appops set --uid "$UID" WRITE_EXTERNAL_STORAGE allow
+if appops get $PKG >/dev/null 2>&1; then
+  pm grant --all-permissions $PKG
+  appops set $PKG SYSTEM_ALERT_WINDOW allow
+  appops set $PKG WRITE_SETTINGS allow
+  appops set $PKG LEGACY_STORAGE allow
+  appops set $PKG READ_EXTERNAL_STORAGE allow
+  appops set $PKG WRITE_EXTERNAL_STORAGE allow
+  appops set $PKG READ_MEDIA_AUDIO allow
+  appops set $PKG READ_MEDIA_VIDEO allow
+  appops set $PKG READ_MEDIA_IMAGES allow
+  appops set $PKG WRITE_MEDIA_AUDIO allow
+  appops set $PKG WRITE_MEDIA_VIDEO allow
+  appops set $PKG WRITE_MEDIA_IMAGES allow
+  appops set $PKG CONTROL_AUDIO allow
+  appops set $PKG CONTROL_AUDIO_PARTIAL allow
   if [ "$API" -ge 29 ]; then
-    appops set --uid "$UID" ACCESS_MEDIA_LOCATION allow
+    appops set $PKG ACCESS_MEDIA_LOCATION allow
+  fi
+  if [ "$API" -ge 30 ]; then
+    appops set $PKG MANAGE_EXTERNAL_STORAGE allow
+    appops set $PKG NO_ISOLATED_STORAGE allow
+    appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+  fi
+  if [ "$API" -ge 31 ]; then
+    appops set $PKG MANAGE_MEDIA allow
+  fi
+  if [ "$API" -ge 33 ]; then
+    appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
   fi
   if [ "$API" -ge 34 ]; then
-    appops set --uid "$UID" READ_MEDIA_VISUAL_USER_SELECTED allow
+    appops set $PKG READ_MEDIA_VISUAL_USER_SELECTED allow
   fi
-  UIDOPS=`appops get --uid "$UID"`
+  PKGOPS=`appops get $PKG`
+  UID=`grep "^$PKG " /data/system/packages.list | awk '{print $2}'`
+  if [ "$UID" ] && [ "$UID" -gt 9999 ]; then
+    appops set --uid "$UID" LEGACY_STORAGE allow
+    appops set --uid "$UID" READ_EXTERNAL_STORAGE allow
+    appops set --uid "$UID" WRITE_EXTERNAL_STORAGE allow
+    if [ "$API" -ge 29 ]; then
+      appops set --uid "$UID" ACCESS_MEDIA_LOCATION allow
+    fi
+    if [ "$API" -ge 34 ]; then
+      appops set --uid "$UID" READ_MEDIA_VISUAL_USER_SELECTED allow
+    fi
+    UIDOPS=`appops get --uid "$UID"`
+  fi
+  pm disable $PKG/.CommonWebAdActivity
+  pm disable $PKG/.joox.view.WebAdActivity
+  pm disable $PKG/com.yandex.mobile.ads.common.AdActivity
+  pm disable $PKG/com.xiaomi.miglobaladsdk.appopenad.OpenNativeAdActivity
+  pm disable $PKG/com.yandex.mobile.ads.core.initializer.MobileAdsInitializeProvider
+  pm disable $PKG/com.google.android.gms.ads.AdService
+  pm disable $PKG/com.amazon.device.ads.DTBInterstitialActivity
+  pm disable $PKG/com.amazon.device.ads.DTBAdActivity
+  pm disable $PKG/com.google.android.gms.ads.AdActivity
+  pm disable $PKG/com.zeus.gmc.sdk.mobileads.msa.adjump.AdJumperLoadingActivity
+  pm disable $PKG/com.zeus.gmc.sdk.mobileads.columbus.ad.videoads.player.GMCAdActivity
+  pm disable $PKG/com.google.android.gms.ads.MobileAdsInitProvider
+  pm disable $PKG/.content.SystemAdContentProvider
+  pm disable $PKG/com.my.target.common.MyTargetActivity
+  pm disable $PKG/com.facebook.ads.AudienceNetworkActivity
+  pm disable $PKG/com.zeus.gmc.sdk.mobileads.columbus.ad.interstitialad.ColumbusActivity
+  pm disable $PKG/com.facebook.ads.AudienceNetworkContentProvider
+  mkdir -p /storage/emulated/"$USR"/Android/data/$PKG/files
 fi
-pm disable $PKG/.CommonWebAdActivity
-pm disable $PKG/.joox.view.WebAdActivity
-pm disable $PKG/com.yandex.mobile.ads.common.AdActivity
-pm disable $PKG/com.xiaomi.miglobaladsdk.appopenad.OpenNativeAdActivity
-pm disable $PKG/com.yandex.mobile.ads.core.initializer.MobileAdsInitializeProvider
-pm disable $PKG/com.google.android.gms.ads.AdService
-pm disable $PKG/com.amazon.device.ads.DTBInterstitialActivity
-pm disable $PKG/com.amazon.device.ads.DTBAdActivity
-pm disable $PKG/com.google.android.gms.ads.AdActivity
-pm disable $PKG/com.zeus.gmc.sdk.mobileads.msa.adjump.AdJumperLoadingActivity
-pm disable $PKG/com.zeus.gmc.sdk.mobileads.columbus.ad.videoads.player.GMCAdActivity
-pm disable $PKG/com.google.android.gms.ads.MobileAdsInitProvider
-pm disable $PKG/.content.SystemAdContentProvider
-pm disable $PKG/com.my.target.common.MyTargetActivity
-pm disable $PKG/com.facebook.ads.AudienceNetworkActivity
-pm disable $PKG/com.zeus.gmc.sdk.mobileads.columbus.ad.interstitialad.ColumbusActivity
-pm disable $PKG/com.facebook.ads.AudienceNetworkContentProvider
-
 
 
 
